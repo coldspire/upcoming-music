b8c142c996204671537ff8fa6d3b26fe66775855
Owen Anderson <5332788+coldspire@users.noreply.github.com>
2024-02-15 22:00:53 -0500

    Add discord.js and stub behavior from discord.js guide

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-require('dotenv').config()
-console.log(process.env.UM_DISCORD_API_KEY);
+require('dotenv').config();
+
+const {env: {UM_DISCORD_TOKEN: DISCORD_TOKEN}} = process;
+const {Client, Events, GatewayIntentBits} = require('discord.js');
+
+const client = new Client({intents: [GatewayIntentBits.Guilds]});
+
+client.once(Events.ClientReady, readyClient => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.login(DISCORD_TOKEN);
