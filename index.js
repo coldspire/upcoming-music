723330f397738e26dc599205b90a1c3f4a26831b
Owen Anderson <5332788+coldspire@users.noreply.github.com>
2024-02-15 22:05:53 -0500

    Add Prettier and Prettier-fy all files

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const {env: {UM_DISCORD_TOKEN: DISCORD_TOKEN}} = process;
-const {Client, Events, GatewayIntentBits} = require('discord.js');
+const {
+  env: { UM_DISCORD_TOKEN: DISCORD_TOKEN },
+} = process;
+const { Client, Events, GatewayIntentBits } = require("discord.js");
 
-const client = new Client({intents: [GatewayIntentBits.Guilds]});
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.once(Events.ClientReady, readyClient => {
-	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+client.once(Events.ClientReady, (readyClient) => {
+  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
 client.login(DISCORD_TOKEN);
