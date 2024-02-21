257f1c6410414f7aec6a8c5ab0cfaf3e1d3c6a7e
Owen Anderson <5332788+coldspire@users.noreply.github.com>
2024-02-20 22:15:18 -0500

    Update upcoming command to get sheets data and show message

--- commands/utility/upcoming.js
+++ commands/utility/upcoming.js
@@ -1,10 +1,13 @@
 const { SlashCommandBuilder } = require("discord.js");
+const getUpcomingMusicValues = require("../../sheets");
+const createMessageFromUpcomingsRaw = require("../../message-maker");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("upcoming")
     .setDescription("Responds with upcoming music releases"),
   async execute(interaction) {
-    await interaction.reply("Upcoming: lol");
+    const upcomingsRaw = await getUpcomingMusicValues();
+    await interaction.reply(createMessageFromUpcomingsRaw(upcomingsRaw));
   },
 };
