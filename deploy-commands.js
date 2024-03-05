5b697d9e6a0d2421ef8c044c579629252c07dac1
Owen Anderson <5332788+coldspire@users.noreply.github.com>
2024-03-04 21:09:42 -0500

    Change environment variables to be pulled via Wrangler

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,36 +1,30 @@
-require("dotenv").config();
-
-const { REST, Routes } = require("discord.js");
-const fs = require("node:fs");
-const path = require("node:path");
+const { REST, Routes } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
 
 const {
-  env: { UM_DISCORD_TOKEN: DISCORD_TOKEN, GUILD_ID, CLIENT_ID },
+	env: { UM_DISCORD_TOKEN: DISCORD_TOKEN, GUILD_ID, CLIENT_ID },
 } = process;
 
 const commands = [];
 // Grab all the command folders from the commands directory you created earlier
-const foldersPath = path.join(__dirname, "commands");
+const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
-  // Grab all the command files from the commands directory you created earlier
-  const commandsPath = path.join(foldersPath, folder);
-  const commandFiles = fs
-    .readdirSync(commandsPath)
-    .filter((file) => file.endsWith(".js"));
-  // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
-  for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
-    if ("data" in command && "execute" in command) {
-      commands.push(command.data.toJSON());
-    } else {
-      console.log(
-        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`,
-      );
-    }
-  }
+	// Grab all the command files from the commands directory you created earlier
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
+	// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		if ('data' in command && 'execute' in command) {
+			commands.push(command.data.toJSON());
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
 }
 
 // Construct and prepare an instance of the REST module
@@ -38,22 +32,15 @@ const rest = new REST().setToken(DISCORD_TOKEN);
 
 // and deploy your commands!
 (async () => {
-  try {
-    console.log(
-      `Started refreshing ${commands.length} application (/) commands.`,
-    );
+	try {
+		console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
-    // The put method is used to fully refresh all commands in the guild with the current set
-    const data = await rest.put(
-      Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
-      { body: commands },
-    );
+		// The put method is used to fully refresh all commands in the guild with the current set
+		const data = await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: commands });
 
-    console.log(
-      `Successfully reloaded ${data.length} application (/) commands.`,
-    );
-  } catch (error) {
-    // And of course, make sure you catch and log any errors!
-    console.error(error);
-  }
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	} catch (error) {
+		// And of course, make sure you catch and log any errors!
+		console.error(error);
+	}
 })();
--- sheets.js
+++ sheets.js
@@ -1,22 +1,20 @@
-require("dotenv").config();
-
-const axios = require("axios").default;
+const axios = require('axios').default;
 
 const {
-  env: { SHEETS_API_KEY, SHEET_ID },
+	env: { SHEETS_API_KEY, SHEET_ID },
 } = process;
 
 function getUpcomingMusicValues() {
-  const range = "All!A4:E100";
-  const url = `https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/${range}`;
+	const range = 'All!A4:E100';
+	const url = `https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/${range}`;
 
-  return axios
-    .get(url, {
-      params: {
-        key: SHEETS_API_KEY,
-      },
-    })
-    .then((response) => response.data.values);
+	return axios
+		.get(url, {
+			params: {
+				key: SHEETS_API_KEY,
+			},
+		})
+		.then((response) => response.data.values);
 }
 
 module.exports = getUpcomingMusicValues;
